72b7b31136dad9f832870175bfee2af25ec807f4
Rock Qu <yunq@xilinx.com>
2017-08-24 10:56:30 -0700

    restructuring repo

--- pynqtutorial/setup.sh
+++ pynqtutorial/setup.sh
@@ -3,7 +3,12 @@
 sudo chown -R xilinx:xilinx /home/xilinx/jupyter_notebooks
 sudo chown -R xilinx:xilinx /opt/python3.6/lib/python3.6/site-packages/pynq
 
-cp -rf ./jupyter_notebooks/pynqtutorial /home/xilinx/jupyter_notebooks/
-cp -rf ./overlays/pynqtutorial /home/xilinx/pynq/overlays/
+cp -rf ./notebooks/pynqtutorial /home/xilinx/jupyter_notebooks/
+
+rm -rf /home/xilinx/pynq/overlays/pynqtutorial
+mkdir /home/xilinx/pynq/overlays/pynqtutorial
+cp -rf ./*.bit /home/xilinx/pynq/overlays/pynqtutorial/
+cp -rf ./*.tcl /home/xilinx/pynq/overlays/pynqtutorial/
+cp -rf ./*.py /home/xilinx/pynq/overlays/pynqtutorial/
 
 echo "Setup Completed"
